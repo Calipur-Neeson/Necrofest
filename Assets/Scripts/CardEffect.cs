a1273222e2793f7db0a672a07cc24d5cfedbc3de
Kastrar <kastrarinall@gmail.com>
2025-02-07 14:06:25 +0200

    cardeffect edits

--- Assets/Scripts/CardEffect.cs
+++ Assets/Scripts/CardEffect.cs
@@ -15,7 +15,7 @@ public class CardEffect : MonoBehaviour
         
     }
 
-    public void  TriggerEffect(string name)
+    public void TriggerEffect(string name)
     {
         switch (name)
         {
@@ -35,8 +35,17 @@ public class CardEffect : MonoBehaviour
                 //base card player dmg (100)
                 break;
             }
-            
+
             //Common cards
+            case "Triforce":
+            {
+                /*
+                 * playerMovementSpeedIncrease(1%)
+                 * playerSpeedIncrease (1%)
+                 * playerAttackSpeedIncrease (1%)
+                 */
+                break;
+            }
             case "Increased Strength":
             {
                 //playerDamageIncrease (5%)
@@ -67,7 +76,7 @@ public class CardEffect : MonoBehaviour
                 //playerJumpHeightIncrease (5%)
                 break;
             }
-            
+
             // Rare cards
             case "Quickdraw":
             {
@@ -79,19 +88,19 @@ public class CardEffect : MonoBehaviour
                     {
                         playerRangedDamageIncrease (20%)
                     }
-                 }                  
+                 }
                  */
                 break;
             }
             case "Chain Swings":
             {
-                
+
                 //Chained melee hits deal extra dmg
-                
+
                 //EnableChainSwings();
                 /*
                 public void ChainSwings()
-                {   
+                {
                  float comboTimer = 3f;
                  event(PlayerAttackMelee)
                     {
@@ -112,18 +121,156 @@ public class CardEffect : MonoBehaviour
                 public void Skirmisher()
                 {
                  event(PlayerAttackRange)
-                 
+                    {
+                        NextPlayerMeleeDamageIncrease (20%)
+                    }
                 }
-                 
-                  
                  */
                 break;
             }
-            case "Triforce":
+
+            case "Blood Tribute":
             {
-                //
+                //Deal extra damage after getting hit
+                //EnableBloodTribute();
+                /*
+                 public void BloodTribute()
+                 {         
+                    public float tributeTime = 3f;                         
+                    event(playerHit)
+                    {
+                        while (time.deltaTime >= time.deltaTime + tributeTime)
+                        {
+                            playerDamageIncrease (30%)
+                        }
+                    }
+                 }
+                 */
+                break;
+            }
+            case "Eagle":
+            {
+                //Extra damage while in air
+                //EnableEagle();
+                /*
+                public void Eagle()
+                {
+                    while (playerNotGrounded)
+                    {
+                        playerDamageIncrease (15%)
+                    }
+                }    
+                */
+                break;
+            }
+            case "Swoop In":
+            {
+                //Extra damage after dash
+                //EnableSwoopIn();
+                /*
+                public void SwoopIn()
+                {
+                    public float swoopTime = 3f;
+                    event (playerDash)
+                        {
+                        while (time.deltaTime =< time.deltaTime + swoopTime)
+                            {
+                                playerDamageIncrease(10%)
+                            }
+                        }
+                }
+                */
+                break;
+            }
+            case "Crunchy Crits":
+            {
+                //Increases critical damage
+                //playerCriticalDamageIncrease (20%)
+                break;
+            }
+            case "Increased Vitality":
+            {
+                //Increase player health
+                //playerHealthIncrease
+                break;
+            }
+            case "Multitude Tap":
+            {
+                //Increase range attack charges per cooldown
+                //playerRangeChargeIncrease (+1)
+                break;
+            }
+            case "Speed Daemon":
+            {
+                //Increase damage based on movement speed
+                //EnableSpeedDaemon();
+                /*
+                public void SpeedDaemon(float playerDamage)
+                {
+                    
+                    float playerMS = Player.CurrentVelocity;
+                    float speedDaemonBonus = playerDamage*1.05^(playerMS-90)/10;
+                }  
+                 */
+                break;
+            }
+
+            //Epic upgrades
+            case "Divine Dash":
+            {
+                //Immunity frames on dash
+                //EnableDivinieDash();
+                /*
+                 
+                 public void EnableDivineDash()
+                 {
+                    Animator animator = GetComponent<Animator>();
+                    animator.enableDashIframes = true;
+                 }
+                 */
+                
+                //In dash animator add animation triggers that 
+                //turns damageable script off and enable after dash finishes.
+                break;
+            }
+            case "Payback":
+            {
+                break;
+            }
+            case "Ricochet":
+            {
+                break;
+            }
+            case "Blood and Pain":
+            {
+                break;
+            }
+            case "Mercy Kill":
+            {
+                break;
+            }
+            case "Deaths Door":
+            {
+                break;
+            }
+            case "Death Cheat":
+            {
+                break;
+            }
+            case "Critical Chain":
+            {
+                break;
+            }
+            case "Deadly Entrance":
+            {
+                break;
+            }
+            case "Parry":
+            {
                 break;
             }
         }
+    
+    
     }
 }
