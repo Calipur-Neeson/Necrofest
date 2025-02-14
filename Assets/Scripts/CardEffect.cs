bb76ee4979684b92ac691fe460bb1224dc5fe172
Kastrar <kastrarinall@gmail.com>
2025-02-14 14:03:19 +0200

    Update CardEffect.cs

--- Assets/Scripts/CardEffect.cs
+++ Assets/Scripts/CardEffect.cs
@@ -235,38 +235,161 @@ public class CardEffect : MonoBehaviour
             }
             case "Payback":
             {
+                //Deal big aoe dmg after being hit (100dmg)
+                //EnablePayback();
+                //Add a sphere trigger collider for player.
+                /*
+                public void Payback()
+                public float paybackDamage = 100f;
+                public GameObject trigger;
+                event(playerHit)
+                {
+                    foreach (enemy in range of trigger)
+                    {
+                        damageEnemy();
+                    }
+                }
+                 */
                 break;
             }
             case "Ricochet":
             {
+                //Range attack bounces to nearby 2 enemies in 2m range
+                //EnableRicochet();
+                /*
+                public void Ricochet()
+                {
+                    
+                    //get enemies in range of main target hit
+                    Enemy[] enemyList = new Enemy[]();
+                    forEeach (enemy in enemyList)
+                    {
+                        //get distance to player
+                        //order closest from furthest to original target
+                        for (int i = 0; i < 1; i++)
+                        {
+                            target = enemyList[i];
+                            dealDamage(target);
+                        }
+                    }
+                }
+                
+                */
                 break;
             }
             case "Blood and Pain":
             {
+                //Take one damage for extra damage
+                //EnableBNP();
+                /*
+                public void BloodAndPain()
+                {
+                    damagePlayer(1);
+                    playerDamageIncrease(); (25%)
+                }
+                */
                 break;
             }
             case "Mercy Kill":
             {
+                //Execute enemies on low hp
+                //EnableMercyKill(); 
+                //mercy kill is enabled on enemy script
+                /*
+                public void FixedUpdate()
+                {
+                   if (MercyKill == true) 
+                        if (hp <= 0 || hp <= maxHp*0.1)
+                            Destroy(this);
+                }
+                */
                 break;
             }
             case "Deaths Door":
             {
+                //Deal extra damage when hp is 1
+                //EnableDeathsDoor();
+                /*
+                public void FixedUpdate()
+                {
+                    if (DeathsDoor == true)
+                        if(hp = 1)
+                            playerDamageIncrease(); (100%)
+                }
+                */
                 break;
             }
             case "Death Cheat":
             {
+                //Negate first death set back to 1 hp, remove card
+                //EnableDeathCheat();
+                /*
+                public void DeathCheat()
+                {
+                        if (hp = 0)
+                        {
+                            hp = 1;
+                            RemoveCard(DeathCheat);
+                            DisableDeathCheat();
+                        }
+                }
+                */
                 break;
             }
             case "Critical Chain":
             {
+                //Increase crit chance after critting till next crit
+                //EnableCriticalChain();
+                /*
+                public void CriticalChain()
+                {
+                    event(playerCrits)
+                    {
+                    private bool CCActive;
+                        if (CCActive = false)
+                            {
+                                playerCritChanceIncrease(); (double crit chance)
+                                CCActive = true;
+                            }
+                        else
+                            {
+                                playerCritChanceDecrease(); (remove doubling)
+                                CCActive = false;
+                            }
+                    }
+                }
+                */
                 break;
             }
             case "Deadly Entrance":
             {
+                //Increase damage for 10 sec after entering room
+                //EnableDeadlyEntrance
+                /*
+                Public Void DeadlyEntrance()
+                {
+                    onTriggerEnter(roomEnterTrigger)
+                    {
+                        while (Time.time >= Time.time + 10)
+                            playerDamageIncrease(); (double dmg)
+                    }
+                }
+                 */
                 break;
             }
             case "Parry":
             {
+                //Deal damage when block triggers
+                //EnableParry();
+                /*
+                Public Void Parry()
+                {
+                    event(PlayerBlocks)
+                    {
+                        //Enemy that attacked takes damage (weapon dmg)
+                    }
+                } 
+                */
                 break;
             }
         }
