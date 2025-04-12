a3657d19fbaf4c96d584ee404826676def93e0ec
Calipur <mycroftn@163.com>
2025-04-12 14:53:38 +0300

    (+)mini map

---
+++ Assets/Scripts/MiniMap/MiniMapManager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MiniMapManager : MonoBehaviour
+{
+    [Header("References")]
+    public RectTransform miniMapPoint_1;
+    public RectTransform miniMapPoint_2;
+    public Transform worldPoint_1;
+    public Transform worldPoint_2;
+}
