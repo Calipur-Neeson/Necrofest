8f0d04bc8c6db3e90d73c0dfa5452cec3298d011
VVeeti-Jussi <veeti.tohni@gmail.com>
2025-04-15 00:55:50 +0300

    keys for animations
    animations works when you press (c,v,b,n)

--- Assets/Scripts/AnimationTest.cs
+++ Assets/Scripts/AnimationTest.cs
@@ -4,11 +4,39 @@ using UnityEngine;
 public class AnimationTest : MonoBehaviour
 {
     [SerializeField] Animator flyingEnemy;
+    [SerializeField] Animator cardNPC;
+    [SerializeField] Animator hayguy;
+    [SerializeField] Animator coffinguy;
+    [SerializeField] Animator boss;
 
-    IEnumerator Animations()
+    public void Update()
     {
-        flyingEnemy.SetBool("isAttacking", true);
-        flyingEnemy.SetFloat("Attack", 1);
-        yield return new WaitForSeconds(2);
+        if (Input.GetKeyDown("c"))
+        {
+            flyingEnemy.SetBool("isAttacking", true);
+            cardNPC.SetBool("isVisible", false);
+            hayguy.SetBool("isAttacking", true);
+            coffinguy.SetBool("isAttacking", true);
+            boss.SetBool("isFastAttacking", true);
+        }
+
+        if (Input.GetKeyDown("v"))
+        {
+            boss.SetBool("isSlowAttacking", true);
+        }
+
+        if (Input.GetKeyDown("b"))
+        {
+            coffinguy.SetBool("isWalking", true);
+            boss.SetBool("isWalking", true);
+        }
+
+        if (Input.GetKeyDown("n"))
+        {
+            flyingEnemy.SetBool("isDead", true);
+            hayguy.SetBool("isDead", true);
+            coffinguy.SetBool("isDead", true);
+            boss.SetBool("isDead", true);
+        }
     }
 }
