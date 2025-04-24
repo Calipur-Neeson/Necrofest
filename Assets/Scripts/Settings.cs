e2d7b27a2dd2d9290eb4eb585ee77de4f85a9434
VVeeti-Jussi <veeti.tohni@gmail.com>
2025-04-24 15:12:05 +0300

    settings and scaling
    added fullscreen option, resolutions and fixed scaling for build

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
-    public Dropdown resolutionDropdown;
+    //public Dropdown resolutionDropdown;
+    public TMPro.TMP_Dropdown resolutionDropdown;
 
     Resolution[] resolutions;
     private void Start()
@@ -16,13 +17,27 @@ public class Settings : MonoBehaviour
 
        List<string> options = new List<string>();
 
+        int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
             string option = resolutions[i].width + " x " + resolutions[i].height;
             options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
         }
 
         resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
     public void SetQuality (int qualityIndex)
     {
